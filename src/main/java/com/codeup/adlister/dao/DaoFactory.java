096e45c86cd47443334a63d90347f0491b650f69
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2023-01-09 13:26:21 -0700

    correcting main branch

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -1,15 +1,15 @@
 package com.codeup.adlister.dao;
 
-import javax.servlet.jsp.jstl.core.Config;
+
 
 public class DaoFactory {
     private static Ads adsDao;
     private static Users usersDao;
-    private static Config config = new Config();
 
     public static Ads getAdsDao() {
+         Config config = new Config();
         if (adsDao == null) {
-            adsDao = new MySQLAdsDao();
+            adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
     }
